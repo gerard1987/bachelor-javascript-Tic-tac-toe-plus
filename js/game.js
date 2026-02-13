74edfd0e2240796e7871ca51f65228c2ea694242
DESKTOP-NTB6DL0\katan <gerard.taa@gmail.com>
2026-02-13 13:49:03 +0100

    Initial OOP game and board initialization

--- js/game.js
+++ js/game.js
@@ -1,51 +1,181 @@
 class TicTacToe {
-  constructor(size) {
-    this.size = size;
-    console.log(this.size);
-    console.log(size.cells);
+  constructor() {
+    this.playElement = document.getElementById('btn-start');
+    this.resetElement = document.getElementById('btn-rest');
+    this.widthElement = document.getElementById('input-width');
+    this.heightElement = document.getElementById('input-height');
+    this.winConditionElement = document.getElementById('input-win-condition');
+    this.boardElement = document.getElementById('board');
+
+    this.boardWidth = this.parseInputValue(this.widthElement.value);
+    this.boardHeight = this.parseInputValue(this.heightElement.value);
+    this.winCondition = this.parseInputValue(this.winConditionElement.value);
+    this.players = [];
+    this.players.push(new Player('x'));
+    this.players.push(new Player('o'));
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.widthElement.addEventListener('change', (event) => {
+      this.boardWidth = this.parseInputValue(event.target.value);
+      this.newGame();
+    });
+
+    this.heightElement.addEventListener('change', (event) => {
+      this.boardHeight = this.parseInputValue(event.target.value);
+      this.newGame();
+    });
+
+    this.winConditionElement.addEventListener('change', (event) => {
+      this.winCondition = this.parseInputValue(event.target.value);
+      this.newGame();
+    });
+  }
+
+  parseInputValue(value) {
+    return parseInt(value, 10);
+  }
+
+  newGame() {
+    this.game = new Game(
+      new Board(
+        this.boardElement,
+        this.boardHeight,
+        this.boardWidth,
+      ),
+      this.players
+    )
+
+    this.game.init();
+  }
+}
+
+class Player {
+  constructor(type) {
+    this.type = type;
+  }
+}
+
+class Game {
+  constructor(board, players) {
+    this.board = board
+    this.players = players;
+    this.setCurrentPlayer(
+      this.setStartingPlayer()
+    );
+    console.log(`Initialized game board with ${this.board.rowCount} rows and ${this.board.colCount} cols`);
+    console.log(`Current player is ${this.currentPlayer.type}`);
   }
 
   init() {
+    this.board.clear();
+    this.board.render();
+  }
+
+  setStartingPlayer() {
+    return this.players[Math.floor(Math.random() * this.players.length)];
+  }
+
+  setCurrentPlayer(player) {
+    this.currentPlayer = player;
+    let header = document.getElementById('current_player');
+    if (header == null){
+      header = document.createElement('h1');
+      header.id = 'current_player';
+    }
+
+    header.innerHTML = `Player ${this.currentPlayer.type} turn`;
+
+    const gameEl = this.board.boardElement.parentElement;
+    gameEl.appendChild(header);
+  }
+}
+
+class Board {
+  constructor(boardElement, rowCount, colCount) {
+    this.boardElement = boardElement;
+    this.rowCount = rowCount;
+    this.colCount = colCount;
+  }
+
+  clear() {
+    this.boardElement.innerHTML = "";
   }
 
   render() {
-    for (let row = 0; row < this.size; row++) {
-      for (let col = 0; col < this.size; col++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.dataset.row = row;
-        cell.dataset.col = col;
-        cell.textContent = this.board[row][col] || "";
-        cell.addEventListener("click", () => this.handleMove(row, col));
-        this.boardElement.appendChild(cell);
+    // Generate board rows
+    for (let i = 0; i < this.rowCount; i++) {
+
+      const row = new Row(i);
+      const htmlRow = row.generateHtml();
+      this.boardElement.appendChild(htmlRow);
+
+      // Initialize row cols
+      for (let col = 0; col < this.colCount; col++) {
+        row.addCell()
       }
+
+      row.generateCells();
     }
   }
 }
 
-class BoardSize {
-    constructor (width, height){
-        this.width = width;
-        this.height = height;
-        this.cells = this.calculateTotalCells();
-    }
+class Row {
+  constructor(id) {
+    this.id = id;
+    this.cellCount = 0;
+    this.cells = [];
+  }
 
-    calculateTotalCells(){
-        return this.width * this.height;
-    }
+  generateHtml() {
+    const row = document.createElement("div");
+    row.classList.add('row');
+    row.id = `row_${this.id}`;
+    return row;
+  }
+
+  addCell() {
+    const cell = new Cell(this.id, this.cellCount++);
+    this.cells.push(cell);
+  }
+
+  generateCells() {
+    this.cells.forEach(cell => {
+      cell.generateHtml();
+    });
+  }
 }
 
-let game;
+class Cell {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
 
-const playElement = document.getElementById('btn-start');
-const resetElement = document.getElementById('btn-rest');
-const widthElement = document.getElementById('input-width');
-const heightElement = document.getElementById('input-height');
-const winConditionElement = document.getElementById('input-win-condition');
+  generateHtml() {
+    const row = document.getElementById(`row_${this.row}`);
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.id = `cell_${this.col}`;
+    cell.dataset.row = this.row;
+    cell.dataset.col = this.col;
+    cell.textContent = "";
+    cell.addEventListener("click", (event) => this.setCellValue(event));
+    row.appendChild(cell);
+  }
 
-playElement.addEventListener("click", () => {
-  const width = parseInt(widthElement.value, 10);
-  const height = parseInt(heightElement.value, 10);
-  let size = new BoardSize(width, height);
-  game = new TicTacToe(size);
+  setCellValue(event) {
+    const cell = event.target;
+    if (!cell.classList.contains('o') && !cell.classList.contains('o')) {
+      cell.classList.add('o');
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize 
+  const ticTacToe = new TicTacToe();
+  ticTacToe.newGame();
 });
